b0a11b01e0c8c56d9c378542ac4f331a0038b31b
Alejandro Arteaga Chaurra <aarteagac@unal.edu.co>
2026-05-14 11:22:53 -0500

    actualizando el gitignore

--- microcontroller_programming/Src/main.c
+++ microcontroller_programming/Src/main.c
@@ -22,9 +22,48 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+/* Variables del sistema */
+uint8_t dummy_8bit = 0;
+uint16_t dummy_16bit = 0;
+uint32_t dummy_32bit = 0;
+uint8_t overflow = 0;
+
+uint16_t dummy_16bit_dec = 0;
+uint16_t dummy_16bit_bin = 0;
+uint16_t dummy_16bit_hex = 0;
+
+
 int main(void)
 {
-    /* Loop forever */
+
+	dummy_8bit = 123;
+	dummy_16bit = 4986;
+	dummy_32bit = 12345678;
+
+	dummy_16bit_dec = 32;
+	dummy_16bit_bin = 0b100000;
+	dummy_16bit_hex = 0x20;
+
+	/* Cargando el valor en xxxx de la variable yyy */
+	dummy_16bit_bin = dummy_16bit_bin << 3; // prediccion: 0b100000000 = 256
+	dummy_16bit_bin = dummy_16bit_bin >> 3; // prediccion 0b100 = 4 ---------- Mala prediccion
+
+	/*Caso de overflow*/
+	dummy_8bit = 255;
+	dummy_16bit = 255;
+	dummy_32bit = 255;
+
+	/* Incremento el valor de la variable dummy_8bit en 1 y lo cargo en la variable overflow*/
+	overflow = dummy_8bit + 1;
+	overflow = overflow +1;
+	overflow = 735;
+	overflow = 0;
+
+	for(uint16_t counter = 0; counter < 735; counter++){
+		overflow++;
+	}
+
+	/* Loop forever */
 	while(1){
 
 	}
