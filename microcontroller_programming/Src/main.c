18bcb90243a956ed2efb66fb0ca821882152fdc0
Alejandro Arteaga Chaurra <aarteagac@unal.edu.co>
2026-06-02 11:02:01 -0500

    actualizando trabajo semana 4

--- microcontroller_programming/Src/main.c
+++ microcontroller_programming/Src/main.c
@@ -62,29 +62,66 @@ int main(void)
 
 	for(uint16_t counter = 0; counter < 735; counter++){
 		overflow++;
-
-	//RCC->AHB1ENR |= (1 << 0);  Activar señal de reloj
+	}
+	// Activar señal de reloj
+	// RCC->AHB1ENR |= (1 << 0);
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-	// Configurar pin A5 como
-	GPIOA->MODER |= (0b01 << GPIO_MODER_MODE5_Pos);
-
-	// Configurar pin A5 como
-	GPIOA->OTYPER &= -(GPIO_OTYPER_OT5);
-
-	// Limpiando la posicion de los bits que deseo borrar
-	GPIOA->OSPEEDR &= -(0b11 << GPIO_OSPEEDER_OSPEEDR5_Pos);
-
-	// Seleccionando la velocidad -- fast
-	GPIOA->OSPEEDR |= -(0b10 << GPIO_OSPEEDER_OSPEEDR5_Pos);
-
-	// Escribir un 1 en la posicion 5 -> encender el LED2
-	GPIOA->ODR |= (GPIO_ODR_OD5);
-
-
-
-
-	}
+	// Limpiando la posicion primero, por posibles valores previos
+//	GPIOA->OSPEEDR &= ~(0b11 << GPIO_OSPEEDER_OSPEEDR5_Pos);
+//
+//	// Configurar pin A5 como salida
+//	GPIOA->MODER |= (0b01 << GPIO_MODER_MODE5_Pos);
+//
+//	// Configurar pin A5 como salida push-pull
+//	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT5);
+//
+//	// Seleccionando la velocidad -- fast
+//	GPIOA->OSPEEDR |= ~(0b10 << GPIO_OSPEEDER_OSPEEDR5_Pos);
+//
+//	// Escribir un 1 en la posicion 5 -> encender el LED2
+//	//GPIOA->ODR |= (GPIO_ODR_OD5);
+//
+//
+//	// Configurando el TIM3
+//	// Limpiando la posicion TIM3EN
+//	RCC->APB1ENR &= ~(RCC_APB1ENR_TIM3EN);
+//
+//	// Activamos señal de reloj para el TIM3
+//	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+//
+//	// Para generar una señal de 250ms
+//	TIM3->ARR = 249;
+//
+//	// La señal que incrementa el CNT es de 1KHz
+//	TIM3->PSC = 15999;
+//
+//	// Reiniciamos el CNT (aunque no es necesario)
+//	TIM3->CNT = 0;
+//
+//	// Configuramos para que el TIM3 cuente de fomra ascendente
+//	TIM3->CR1 &= ~(TIM_CR1_DIR);
+//
+//	// Limpiamos la posicion ARPE para tenerla en un estado conocido
+//	TIM3->CR1 &= ~(TIM_CR1_ARPE);
+//
+//	// Activamos el registro de precarga del ARR
+//	TIM3->CR1 |= TIM_CR1_ARPE;
+//
+//	// Activamos la IRQ del TIM3 para que el NVIC reciba señales de ella
+//	__NVIC_EnableIRQ(TIM3_IRQn);
+//
+//	// Bajamos la bandera de la interrupcion del TIM3
+//	TIM3->SR &= ~(TIM_SR_UIF);
+//
+//	//Limpiamos la posicion UIE del registro para garantizar un estado conocido
+//	TIM3->DIER &= ~(TIM_DIER_UIE);
+//
+//	// Activamos la interrupcion de actualizacion del TIM3
+//	TIM3->DIER |= TIME_DIER_UIE;
+//
+//	// Ponemos a 1 en CEN, de forma que el TIM3 comience a contar
+//	TIM3->CR1 |= TIM_CR1_CEN;
 
 	/* Loop forever */
 	while(1){
